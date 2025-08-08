d8c2a359074fea2f8d7c5212268281bb58b5b24c
Shahmir Varqha <sham9871@gmail.com>
2025-08-08 22:29:00 +0800

    export default tooltip renderers

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ export type {
   SqlHoverConfig,
   SqlKeywordInfo,
 } from "./sql/hover.js";
-export { defaultSqlHoverTheme, sqlHover } from "./sql/hover.js";
+export { DefaultSqlTooltipRenders, defaultSqlHoverTheme, sqlHover } from "./sql/hover.js";
 export { NodeSqlParser } from "./sql/parser.js";
 export type { SqlStatement } from "./sql/structure-analyzer.js";
 export { SqlStructureAnalyzer } from "./sql/structure-analyzer.js";
