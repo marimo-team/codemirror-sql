e0bb3708f94bbe69c3d18e6ca7306072657d9eea
Shahmir Varqha <sham9871@gmail.com>
2025-11-01 19:40:29 +0800

    add test

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+import { playwright } from "@vitest/browser-playwright";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   root: process.env.VITEST ? "." : "demo",
   test: {
-    environment: "jsdom",
     watch: false,
     coverage: {
       provider: "v8",
@@ -11,6 +11,35 @@ export default defineConfig({
       include: ["src/**/*.ts"],
       exclude: ["src/**/*.test.ts", "src/**/__tests__/**", "src/debug.ts"],
     },
+    projects: [
+      {
+        test: {
+          environment: "jsdom",
+          exclude: ["vitest-example/**"],
+          include: ["src/**/*.test.ts", "src/**/__tests__/**/*.ts"],
+        }
+      },
+      {
+        test: {
+          name: "browser",
+          browser: {
+            enabled: true,
+            provider: playwright(),
+            // https://vitest.dev/guide/browser/playwright
+            instances: [
+              { browser: 'chromium' },
+            ],
+            ui: false,
+            headless: true,
+          },
+          include: ["src/**/*.test.ts", "src/**/__tests__/**/*.ts", "vitest-example/**/*.test.ts"],
+          exclude: ["src/__tests__/index.test.ts"],
+          testTimeout: 5000,
+          hookTimeout: 5000,
+          teardownTimeout: 5000,
+        },
+      }
+    ]
   },
   base: "/codemirror-sql/",
 });
